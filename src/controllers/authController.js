59f1c87c7809d6b45c1f1291b7bc78eb4a905491
JoaoSGabriel <joaogabriel0359@gmail.com>
2022-09-13 10:17:00 -0300

    implement sign-up and sign-in route

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,18 +1,56 @@
 import { v4 as uuid } from 'uuid';
 import bcrypt from 'bcrypt';
 import db from '../db.js';
+import { signUp_Schema, signIn_Schema } from '../schemas/authSchema.js';
 
 async function signUp(req, res) {
+    const { name, email, password, repeat_Password } = req.body;
+
+    const validation = signUp_Schema.validate(req.body, { abortEarly: false });
+    if (validation.error) {
+        return res.status(401).send(validation.error.details.map(value => value.message));
+    }
+
+    const crypt_Password = bcrypt.hashSync(password, 10);
+
     try {
-        res.send('Rota ativa');
+        const user = await db.collection("users").findOne({ name });
+        if (user === null) {
+            await db.collection("users").insertOne({
+                name,
+                email,
+                password: crypt_Password
+            });
+            return res.sendStatus(201);
+        } else {
+            return res.sendStatus(409);
+        }
     } catch (error) {
         return res.status(500).send(error.message);
     }
 }
 
 async function signIn(req, res) {
+    const { email, password } = req.body;
+
+    const validation = signIn_Schema.validate(req.body, { abortEarly: false });
+    if (validation.error) {
+        return res.status(401).send(validation.error.details.map(value => value.message));
+    }
+
     try {
-        res.send('Rota ativa');
+        const user = await db.collection("users").findOne({ email });
+        if (user && bcrypt.compareSync(password, user.password)) {
+            const token = uuid();
+            const session = {
+                userID: user._id,
+                token
+            }
+            await db.collection("sessions").insertOne(session);
+            return res.status(200).send(session);
+        } else {
+            return res.sendStatus(404);
+        }
     } catch (error) {
         return res.status(500).send(error.message);
     }
