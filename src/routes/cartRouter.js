717b3264e880ae7ee4e8a1aca01475093630d3ab
JoaoSGabriel <joaogabriel0359@gmail.com>
2022-09-13 09:14:48 -0300

    fix bug name route

--- src/routes/cartRouter.js
+++ src/routes/cartRouter.js
@@ -6,6 +6,6 @@ const cartRouter = Router();
 cartRouter.post('/cart', addCartProducts);
 cartRouter.get('/cart', getCartProducts);
 cartRouter.delete('/cart/:id', removeCartProducts);
-cartRouter.delete('/cart', cleanCart);
+cartRouter.delete('/cart/all', cleanCart);
 
 export default cartRouter;
