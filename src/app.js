68a9754f8674373e30625745a7791d4758fac04e
Hugo Gomes <gomes.hugo00@gmail.com>
2022-09-13 15:27:45 -0300

    Ajustes authController

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
-import express from 'express';
-import cors from 'cors';
-import router from './routes/indexRouter.js';
+import express from "express";
+import cors from "cors";
+import router from "./routes/indexRouter.js";
 
 const app = express();
 app.use(cors());
